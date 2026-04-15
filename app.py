81ec09e906f346d29fe6b34ff5a8c02af08d4f02
Keisuke <telcation@gmail.com>
2026-04-15 15:41:14 +0900

    initial template

---
+++ app.py
@@ -0,0 +1,10 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World! デプロイ成功！'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=52XX)  # XX を自分のポート番号に変更
